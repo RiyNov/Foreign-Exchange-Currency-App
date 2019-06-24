30831dac1c09de95ecc84b2ab3cfcbac52d56e51
Riyandi Novian <43406693+RiyNov@users.noreply.github.com>
2019-06-24 07:36:07 +0700

    Add files via upload

---
+++ precache-manifest.e3085da61aeef189e78e58afc9226d93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573e5d0907103bddf6908279467c71bb",
+    "url": "/Foreign-Exchange-Currency-App/index.html"
+  },
+  {
+    "revision": "a22862ca015e22ccc811",
+    "url": "/Foreign-Exchange-Currency-App/static/css/main.81a563fb.chunk.css"
+  },
+  {
+    "revision": "163f3088c023cec04804",
+    "url": "/Foreign-Exchange-Currency-App/static/js/2.57b22aa1.chunk.js"
+  },
+  {
+    "revision": "a22862ca015e22ccc811",
+    "url": "/Foreign-Exchange-Currency-App/static/js/main.239bb57f.chunk.js"
+  },
+  {
+    "revision": "1e31f3113bdc5fa5b292",
+    "url": "/Foreign-Exchange-Currency-App/static/js/runtime~main.0e1f49b9.js"
+  }
+]);
